5d1a047ae1650fef408bde1078a2f20444486b80
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-07-06 23:46:21 +0500

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,28 @@ import Section_4 from "./Section_4";
 import Section_5 from "./Section_5";
 import Section_6 from "./Section_6";
 import Section_7 from "./Section_7";
+import { BrowserRouter, Route, Routes } from "react-router";
 function App() {
   return (
     <>
-      <Nav />
-      <Section_1 />
-      <Section_2 />
-      <Section_3 />
-      <Section_4 />
-      <Section_5 />
-      <Section_6 />
-      <Section_7 />
-      <hr style={{
-        border: "1px solid #646464"
-      }} />
-      <Footer />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/home" element={<Section_1 />}></Route>
+          <Route path="/services" element={<Section_2 />}></Route>
+          <Route path="/about" element={<Section_3 />}></Route>
+          <Route path="/since" element={<Section_4 />}></Route>
+          <Route path="/testimonials" element={<Section_5 />}></Route>
+          <Route path="/news" element={<Section_6 />}></Route>
+          <Route path="/contact" element={<Section_7 />}></Route>
+        </Routes>
+        <hr
+          style={{
+            border: "1px solid #646464",
+          }}
+        />
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,18 +1,19 @@
-import React from "react";
 import logo from "./assets/Screenshot_3 [Recovered] 1.svg";
-import "./styles/Nav.scss"
+import "./styles/Nav.scss";
+import { NavLink } from "react-router-dom";
 const Nav = () => {
+
   return (
     <nav>
       <img src={logo} />
       <div className="navigation">
-        <p className="isActive">HOME</p>
-        <p>PROJECT</p>
-        <p>SERVICES</p>
-        <p>ABOUT</p>
-        <p>BLOG</p>
-        <p>SHOP</p>
-        <p>CONTACT</p>
+        <NavLink to="/home" className={({ isActive }) => isActive ? "navLink isActive" : "navLink"}> <p >HOME</p></NavLink>
+        <NavLink to="/services"className={({ isActive }) => isActive ? "navLink isActive" : "navLink"} > <p>SERVICES</p></NavLink>
+        <NavLink to="/about" className={({ isActive }) => isActive ? "navLink isActive" : "navLink"}> <p>ABOUT US</p></NavLink>
+        <NavLink to="/since" className={({ isActive }) => isActive ? "navLink isActive" : "navLink"}> <p >SINCE</p></NavLink>
+        <NavLink to="/testimonials" className={({ isActive }) => isActive ? "navLink isActive" : "navLink"}> <p >TESTIMONIALS</p></NavLink>
+        <NavLink to="/news" className={({ isActive }) => isActive ? "navLink isActive" : "navLink"}> <p>NEWS</p></NavLink>
+        <NavLink to="/contact" className={({ isActive }) => isActive ? "navLink isActive" : "navLink"}> <p>CONTACT US</p></NavLink>
       </div>
       <div className="navButton">Sign Up</div>
     </nav>
